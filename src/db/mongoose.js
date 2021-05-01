4c382222c2167ff79417e0711e08b2d548e31401
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-05-01 23:49:07 +0200

    Code styling

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
 const connectionURL = `mongodb://127.0.0.1:27017/${process.env.TASK_MANAGER_API_ENDPOINT}`
+
 const connectionOptions = {
+    useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useUnifiedTopology: true
 }
 
 mongoose.connect(connectionURL, connectionOptions)
--- src/index.js
+++ src/index.js
@@ -4,61 +4,15 @@ require('dotenv').config()
 require('./db/mongoose')
 
 // Models
-const UserModel = require('./models/user')
 const TaskModel = require('./models/task')
+const UserModel = require('./models/user')
 
 // Express app
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// App config
 app.use(express.json())
-
-// Users post endpoint
-app.post('/users', (req, res) => {
-    const user = new UserModel(req.body)
-
-    user.save()
-        .then(() => res.status(201).send(user))
-        .catch(e => {
-            res.status(400).send({
-                error: e.message,
-                fullMessage: e
-            })
-        })
-})
-
-// Users all route
-app.get('/users', (req, res) => {
-    UserModel.find({})
-        .then(users => res.send(users))
-        .catch(e => {
-            res.status(400).send({
-                error: e.message,
-                fullMessage: e
-            })
-        })
-})
-
-// User route
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id
-
-    UserModel.findById(_id)
-        .then(user => {
-            if (!user) {
-                return res.status(404).send({
-                    message: `UserID: '${_id}' couldn't be found`
-                })
-            }
-            res.send(user)
-        })
-        .catch(e => {
-            res.status(500).send({
-                error: e.message,
-                fullMessage: e
-            })
-        })
-})
 
 // Tasks post endpoint
 app.post('/tasks', (req, res) => {
@@ -74,7 +28,7 @@ app.post('/tasks', (req, res) => {
         })
 })
 
-// Tasks all route
+// List all tasks
 app.get('/tasks', (req, res) => {
     TaskModel.find({})
         .then(tasks => res.send(tasks))
@@ -86,7 +40,7 @@ app.get('/tasks', (req, res) => {
         })
 })
 
-// Task route
+// Reads specific task by ID
 app.get('/tasks/:id', (req, res) => {
     const _id = req.params.id
 
@@ -108,6 +62,54 @@ app.get('/tasks/:id', (req, res) => {
         })
 })
 
+// Users post endpoint
+app.post('/users', (req, res) => {
+    const user = new UserModel(req.body)
+
+    user.save()
+        .then(() => res.status(201).send(user))
+        .catch(e => {
+            res.status(400).send({
+                error: e.message,
+                fullMessage: e
+            })
+        })
+})
+
+// List all users
+app.get('/users', (req, res) => {
+    UserModel.find({})
+        .then(users => res.send(users))
+        .catch(e => {
+            res.status(400).send({
+                error: e.message,
+                fullMessage: e
+            })
+        })
+})
+
+// Reads specific user by ID
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    UserModel.findById(_id)
+        .then(user => {
+            if (!user) {
+                return res.status(404).send({
+                    message: `UserID: '${_id}' couldn't be found`
+                })
+            }
+
+            res.send(user)
+        })
+        .catch(e => {
+            res.status(500).send({
+                error: e.message,
+                fullMessage: e
+            })
+        })
+})
+
 // Page not found route
 app.get('*', (req, res) => {
     res.send({
@@ -116,7 +118,7 @@ app.get('*', (req, res) => {
     })
 })
 
-// Run app
+// Run at PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
