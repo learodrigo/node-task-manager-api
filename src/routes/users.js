649151c9b83b2f0eaceb32752d8e95d8ee92ef9a
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-05-08 18:55:09 +0200

    Handling error in multer

--- src/routes/users.js
+++ src/routes/users.js
@@ -114,6 +114,8 @@ const upload = multer({
 
 usersRouter.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 /**
